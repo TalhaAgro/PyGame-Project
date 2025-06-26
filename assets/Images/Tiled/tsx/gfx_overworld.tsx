df68c1f86cfb88ad00610d9026fc3ad0cf384b33
Talha Agro <talha.agro50@gmail.com>
2025-06-26 13:08:51 +0500

    update assets and uv init

---
+++ assets/Images/Tiled/tsx/gfx_overworld.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="gfx_overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="../../gfx/gfx/Overworld.png" width="640" height="576"/>
+ <wangsets>
+  <wangset name="Grass" type="corner" tile="-1">
+   <wangcolor name="GrasswithSand" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="120" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="121" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="122" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="160" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="161" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="162" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="200" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="201" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="202" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="240" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="241" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="280" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="281" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+  <wangset name="Pavement" type="corner" tile="-1">
+   <wangcolor name="Path3D" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="Path2D" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="498" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="499" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="500" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="538" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="539" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="540" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="578" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="579" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="580" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="613" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="614" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="617" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="618" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="653" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="654" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="657" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="658" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="694" wangid="0,0,0,2,0,0,0,0"/>
+   <wangtile tileid="695" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="696" wangid="0,0,0,0,0,2,0,0"/>
+   <wangtile tileid="734" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="735" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="736" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="774" wangid="0,2,0,0,0,0,0,0"/>
+   <wangtile tileid="775" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="776" wangid="0,0,0,0,0,0,0,2"/>
+   <wangtile tileid="813" wangid="0,2,0,0,0,2,0,2"/>
+   <wangtile tileid="814" wangid="0,2,0,2,0,0,0,2"/>
+   <wangtile tileid="853" wangid="0,0,0,2,0,2,0,2"/>
+   <wangtile tileid="854" wangid="0,2,0,2,0,2,0,0"/>
+  </wangset>
+  <wangset name="Stone" type="corner" tile="-1">
+   <wangcolor name="Stone Bricks" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="455" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="456" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="457" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="495" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="496" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="497" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="535" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="536" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="537" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="575" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="576" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="615" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="616" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+  <wangset name="Trees" type="corner" tile="-1">
+   <wangcolor name="Trees" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="642" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="643" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="644" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="645" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="646" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="647" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="685" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="686" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="687" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="726" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="727" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="763" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="764" wangid="0,1,0,0,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ assets/Images/Tiled/tsx/red collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="red collision" tilewidth="16" tileheight="16" tilecount="192" columns="12">
+ <image source="../../red.jpeg" width="194" height="259"/>
+</tileset>
